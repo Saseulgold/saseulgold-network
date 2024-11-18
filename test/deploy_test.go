09279ea6afa700fd9440ea3dae980088459bb3d5
louis <yerang040231@gmail.com>
2024-11-18 19:28:28 +0900

    add: deploy_test.go file

---
+++ test/deploy_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	abi "hello/pkg/core/abi"
+	C "hello/pkg/core/config"
+	. "hello/pkg/core/model"
+	. "hello/pkg/core/vm"
+	. "hello/pkg/core/vm/native"
+	"testing"
+)
+
+func TestDeploy(t *testing.T) {
+	C.CORE_TEST_MODE = true
+	C.DATA_TEST_ROOT_DIR = "register_test"
+
+	userMethod := &Method{
+		Name:    "userMethod",
+		Version: "1",
+		Parameters: Parameters{
+			"value1": NewParameter(map[string]interface{}{
+				"name":         "value1",
+				"type":         "string",
+				"maxlength":    10,
+				"requirements": true,
+				"default":      "10",
+			}),
+			"value2": NewParameter(map[string]interface{}{
+				"name":         "value2",
+				"type":         "string",
+				"maxlength":    10,
+				"requirements": true,
+				"default":      "20",
+			}),
+		},
+		Executions: []Execution{
+			abi.And(
+				abi.Gt(abi.Param("value1"), "5"),
+				abi.Lt(abi.Param("value2"), "30"),
+			),
+			abi.Or(
+				abi.Eq(abi.Param("value1"), "5"),
+				abi.Gt(abi.Param("value2"), "15"),
+			),
+			abi.And(
+				abi.Or(
+					abi.Gt(abi.Param("value1"), "5"),
+					abi.Lt(abi.Param("value2"), "10"),
+				),
+				abi.Eq(abi.Param("value1"), abi.Param("value2")),
+			),
+		},
+	}
+
+	code := userMethod.GetCode()
+	if code == "" {
+		t.Error("Failed to get user method code")
+	}
+
+	fmt.Println("User Method Code:", code)
+
+	publishMethod := Publish()
+
+	interpreter := NewInterpreter()
+	interpreter.Init("transaction")
+
+	signedData := NewSignedData()
+	signedData.SetAttribute("code", code)
+
+	post := &Method{Parameters: Parameters{},
+		Executions: []Execution{},
+	}
+
+	interpreter.Reset()
+	interpreter.SetSignedData(signedData)
+	interpreter.SetCode(publishMethod)
+	interpreter.SetPostProcess(post)
+
+	msg, _ := interpreter.Execute()
+	t.Logf("Error message: %v", msg)
+
+	/**
+	prefix := "contract"
+	statusHash := F.StatusHash(publishMethod.GetWriter(), publishMethod.GetSpace(), prefix, publishMethod.GetName())
+	t.Logf("Status hash: %v", interpreter.GetLocalStatus(statusHash, nil))
+	*/
+
+}
